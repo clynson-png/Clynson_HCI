0243c50dd6bdac01377576c9aa708537620e3cb8
HCI Taurus <hci-taurus@example.local>
2026-07-01 15:31:03 -0300

    Redirect root Pages to TAURUS mobile

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,11 @@
-const CACHE_NAME = "hci-happy-forms-v1";
-const FILES = ["./", "./index.html", "./manifest.json"];
+self.addEventListener('install', (event) => {
+  event.waitUntil(self.skipWaiting())
+})
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES)));
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
-  );
-});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
+  )
+})
